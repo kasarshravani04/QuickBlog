4b6cb8455357c3686d86f2875ebb79f8e77c8d70
kasarshravani04 <shravanikasar5@gmail.com>
2026-05-21 13:48:08 +0530

    feat:added bloglist section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
+import Bloglist from '../components/Bloglist';
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <>
       <Navbar/>
       <Header/>
+      <Bloglist/>
     </>
   )
 }
