c05c59c6c31548673795d2cf1f2fa76a6d5e356b
kasarshravani04 <shravanikasar5@gmail.com>
2026-05-21 15:47:08 +0530

    feat:added blogcards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import Bloglist from '../components/Bloglist';
+import BlogCard from '../components/BlogCard';
 
 
 const Home = () => {
