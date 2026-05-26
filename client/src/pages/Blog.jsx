3b8b64cf39388aae19172fb465db84278ecc0ffe
kasarshravani04 <shravanikasar5@gmail.com>
2026-05-26 15:14:54 +0530

    feat:create a loader page

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -4,6 +4,7 @@ import { assets, blog_data, comments_data } from '../assets/assets';
 import Navbar from '../components/Navbar';
 import Moment from 'moment';
 import Footer from '../components/Footer';
+import { Loader } from '../components/Loader';
 
 const Blog = () => {
 
@@ -103,7 +104,7 @@ const Blog = () => {
          </div>
          <Footer/>
     </div>
-  ) : <div>Loading....</div>
+  ) : <Loader/>
 }
 
 export default Blog;
